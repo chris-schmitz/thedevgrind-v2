2276a7011db8f339e29b6a63679ec0431aad9d27
cschmitz <schmitz.chris@gmail.com>
2016-06-06 14:56:27 -0500

    Simplifying header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,7 @@
 <script>
     export default {
         data: () => {
-            return {
-                navigations: [
-                    "About",
-                    "Examples",
-                    "Faux-Blog",
-                    "Contact"
-                ]
-            }
+            return {msg: 'header works'}
         }
     }
 </script>
@@ -37,10 +30,18 @@
 </style>
 
 <template>
+    <!-- 
+        Note I considered rendering this as a vue list, but changed it back
+        to hard coded links. There's not really anything gained from rendering
+        as a list at the moment.
+    -->
     <div class="app-header container flex-container">
         <!-- <img src="http://placekitten.com/150/150" class="col-xs-3"> -->
         <div class="btn-group btn-group-justified nav-button-group">
-            <a v-for="nav in navigations" class="btn btn-primary nav-buttons">{{ nav }}</a>
+            <a class="btn btn-primary nav-buttons">About</a>
+            <a class="btn btn-primary nav-buttons">Examples</a>
+            <a class="btn btn-primary nav-buttons">Faux Blog</a>
+            <a class="btn btn-primary nav-buttons">Contact</a>
         </div>
     </div>
 </template>
